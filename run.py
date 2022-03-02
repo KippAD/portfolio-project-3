9cf0dd47f6b42330e228fc7eaa0b11b3eaa6c211
Kipp AD <kippad@protonmail.com>
2022-03-02 15:41:03 +0000

    Add function to select portal and validate the input data

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-import gspread
-from google.oauth2.service_account import Credentials
-import colorama
-from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
-from termcolor import cprint 
+from termcolor import cprint
 from pyfiglet import figlet_format
+from colorama import Fore, Back, Style
+from google.oauth2.service_account import Credentials
+import gspread
+import colorama
+colorama.init(autoreset=True)
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,6 +17,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('python_voting_system')
 
+
 def display_main_menu():
     """
     Displays main menu with options to move to other areas.
@@ -24,7 +25,28 @@ def display_main_menu():
     cprint(figlet_format("The Voting Station!", font='big'), "green", attrs=["bold"])
     print("Welcome to the Voting Station, this is where votes are cast for the upcoming election!... \n")
     print(f"{Fore.BLUE}Please select your portal:\n")
-    print("1. Voter Portal\n2. Admin Portal")
+    print("1. Voter Portal\n2. Admin Portal\n")
 
+    select_portal()
+
+
+def select_portal():
+    """
+    Validates user input and directs them to selected portal.
+    """
+    while True:
+        try:
+            portal = int(input(f"{Fore.CYAN}Press 1 for Voter Portal or 2 for Admin Portal:"))
+        except ValueError:
+            print(f"{Fore.RED}Incorrect Input: Please press 1 for Voter Portal or 2 for Admin Portal")
+            continue
+        
+        if portal == 1 or portal == 2:
+            break
+        else: 
+            print(f"{Fore.RED}Incorrect Input: Please press 1 for Voter Portal or 2 for Admin Portal")
+    
+    return portal
 
 display_main_menu()
+

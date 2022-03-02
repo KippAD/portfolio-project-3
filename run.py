ac288d72e90709246486e4538cc55e2a34a0a185
Kipp AD <kippad@protonmail.com>
2022-03-02 14:20:40 +0000

    Import colorama, termcolor, and pyfiglet

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+from termcolor import cprint 
+from pyfiglet import figlet_format
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,3 +16,5 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('python_voting_system')
+
+
